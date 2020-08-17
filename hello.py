f2b91c3a9904c03926d9276152657cefcc3226d5
Hiroya_W <e8239@g.maizuru-ct.ac.jp>
2020-08-17 19:46:05 +0900

    :+1: JSONを返してみる

--- hello.py
+++ hello.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
+app.config["JSON_AS_ASCII"] = False  # 日本語文字化け対策
+app.config["JSON_SORT_KEYS"] = False  # ソートをそのまま
 
 
 @app.route("/hello")
 def hello(name=None):
-    name = request.args.get("name")
-    return render_template("hello.html", title="Flask Test", name=name)
+    data = [{"name": "山田"}, {"age": 30}]
+    return jsonify({"status": "OK", "data": data})
 
 
 @app.route("/good")
