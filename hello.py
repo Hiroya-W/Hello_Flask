e685e6f0bbe2ebb35d6aa73eae1c246021decea5
Hiroya_W <e8239@g.maizuru-ct.ac.jp>
2020-08-17 19:35:40 +0900

    :+1: POSTしてHTMLを取得してみる

--- hello.py
+++ hello.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template
-import pymysql
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route("/hello/<name>")
+@app.route("/hello", methods=["POST"])
 def hello(name=None):
+    if request.method == "POST":
+        name = request.form["name"]
+    else:
+        name = "no name."
     return render_template("hello.html", title="Flask Test", name=name)
 
 
@@ -16,4 +19,5 @@ def good():
 
 
 if __name__ == "__main__":
+    # curl -X "POST" "127.0.0.1:5000/hello" -d "name=hiro"
     app.run(debug=True)
